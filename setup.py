b0e2cbe484137a0ea57461d8868a847e5da45685
Andrew Graham-Yooll <andrewgy8@gmail.com>
2017-11-14 20:05:44 +0100

    Updated to python 3 in gitlab ci

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
-        'click'
+        'click',
+        'gunicorn',
+        'requests'
     ],
     setup_requires=[
         'pytest-runner',
